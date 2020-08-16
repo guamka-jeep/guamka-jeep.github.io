428d82f32d95538c249b2f9e100f2b2be20d62cd
guamka-jeep <guamka-jeep@localhost>
2020-08-16 22:46:05 +0300

    fixed email notices

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -39,7 +39,7 @@ $(function() {
           $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
           $('#success > .alert-success')
-            .append("<strong>Your message has been sent. </strong>");
+            .append("<strong>Ваше сообщение отправлено. </strong>");
           $('#success > .alert-success')
             .append('</div>');
           //clear all fields
@@ -51,7 +51,7 @@ $(function() {
           $('#success').html("<div class='alert alert-danger'>");
           $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
             .append("</button>");
-          $('#success > .alert-danger').append($("<strong>").text("Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!"));
+          $('#success > .alert-danger').append($("<strong>").text("Произошла ошибка отправки сообщения, попробуйте связаться по телефону"));
           $('#success > .alert-danger').append('</div>');
           //clear all fields
           $('#contactForm').trigger("reset");
